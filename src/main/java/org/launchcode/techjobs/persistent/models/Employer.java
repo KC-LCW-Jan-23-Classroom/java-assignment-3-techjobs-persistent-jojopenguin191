210bf67d75623c3cdfdb6209309b9ca40775d3eb
jojopenguin191 <107898041+jojopenguin191@users.noreply.github.com>
2023-09-06 19:27:23 -0500

    Model 4.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank (message = "Field cannot be blank")
+    @Size(max = 100, message = "Must contain 100 or fewer characters")
+    private String location;
+
+    public Employer() {
+    }
+
+    //getter
+    public String getLocation() {
+        return location;
+    }
+
+    //setter
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
+
