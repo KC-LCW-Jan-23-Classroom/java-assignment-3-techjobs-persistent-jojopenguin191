6ee1c9cb43ecc8ceee1aa2443bf21c07b8c1bd29
jojopenguin191 <107898041+jojopenguin191@users.noreply.github.com>
2023-09-17 17:37:06 -0500

    EmployerController updates

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,17 +1,29 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @GetMapping("/employers")
+    public String index(Model model) {
+        model.addAttribute("employers", employerRepository.findAll());
+        return "employers/index";
+    }
 
 
     @GetMapping("add")
@@ -25,9 +37,14 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("employer", "Add new employer");
+            model.addAttribute(new Employer());
+
             return "employers/add";
         }
 
+        model.addAttribute(new Employer());
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
 
