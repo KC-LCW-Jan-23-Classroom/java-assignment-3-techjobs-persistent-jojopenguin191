4c8fdea9f575c8f7d777085b176f30ba05d6dcd5
jojopenguin191 <107898041+jojopenguin191@users.noreply.github.com>
2023-09-20 15:54:35 -0500

    Updating Home Controller

--- queries.sql
+++ queries.sql
@@ -1,6 +1,17 @@
 -- Part 1: Test it with SQL
 
+CREATE TABLE job (
+     PRIMARY KEY id INTEGER,
+    employer varchar(255),
+    name varchar(50),
+    skills varchar(255)
+);
+
 -- Part 2: Test it with SQL
+
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,16 +15,22 @@ public class Employer extends AbstractEntity {
     @Size(min = 1, max = 100, message = "Must contain between 1 and 100 characters")
     private String location;
 
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
     public Employer() {
     }
 
     //getter
     public String getLocation() {
+
         return location;
     }
 
     //setter
     public void setLocation(String location) {
+
         this.location = location;
     }
 
